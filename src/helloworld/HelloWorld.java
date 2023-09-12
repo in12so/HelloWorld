c3347f9c892a33c272e0f1b69c96c930826b2081
in12so <73948915+in12so@users.noreply.github.com>
2023-09-12 11:29:31 +0100

    We added Hi

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+
+package helloworld;
+
+/**
+ *
+ * @author IN12SO
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World, again");
+        System.out.println("Hi");
+        
+    }
+    
+}
